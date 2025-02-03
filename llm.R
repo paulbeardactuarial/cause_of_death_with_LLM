f72452c227a6acdae875010948085a1df054a779
Paul Beard <paulbread88@gmail.com>
2025-02-03 22:09:53 +0000

    code developments... JSON method

--- llm.R
+++ llm.R
@@ -1,82 +1,79 @@
-
+library(tidyverse)
 library(ellmer)
 
+# Note for use of the LLM you must have an API with groq. And set GROQ_API_KEY using usethis::edit_r_environ()
+
+# source functions written for this work
+source(".\prompt_writing_functions.R")
+
+# get our causes of death for categorizing
+x <- cod_data$D
+x <- x |> stringr::str_remove_all("[)(:,]")
+
+# get our options for placing into categories
+# the options are taken from the following paper...
+# https://pmc.ncbi.nlm.nih.gov/articles/PMC3229033/#:~:text=Current%20smokers%20had%20significantly%20higher,23.93)%2C%20smoking%2Drelated%20cancers
+options <-
+  c(
+    "coronary heart disease",
+    "cerebrovascular disease",
+    "sudden death",
+    "pulmonary disease",
+    "lung cancer",
+    "colorectal cancer",
+    "larynx cancer",
+    "kidney cancer",
+    "acute myeloid leukemia",
+    "mouth cancer",
+    "esophageal cancer",
+    "pancreatic cancer",
+    "bladder cancer",
+    "stomach cancer",
+    "prostate cancer",
+    "none"
+  )
+
+# get our prompts of x for processing
+list_x <- x |> split_vector(20)
+prompts_list <- list_x |> purrr::map(glue_to_json)
+
+vectors <- seq_along(prompts_list)
+output_list <- vector("list", length = length(prompts_list))
+
+
+# prime the LLM with the intial prompt for classifcation
 # set up llama model via groq
 # to do this, you must have an API with groq. And set GROQ_API_KEY using usethis::edit_r_environ()
-llama_model <- chat_groq(model = "llama-3.3-70b-versatile")
+model <- chat_groq(#model = "llama-3.3-70b-versatile",
+                   model = "llama-3.3-70b-specdec",
+                   system_prompt = write_intial_prompt(options))
 
-delimiter <- "\n"
 
-# function to create send classification request to LLM and return results in vector
-classification_prompt <- function(x, options, model, delimiter = "\n") {
-  
-  # prompt <-
-  # paste(
-  #   "You are a classification AI", 
-  #   "Determine, for each text separated by `,", delimiter,"`, if the text for each item of the input vector refers to one of the following options:",
-  #   paste(options, collapse = ", "), 
-  #   "No capitalization. No explanations. Write only one option for each text.",
-  #   "The answer is based on the following input:",
-  #   paste(x, collapse = delimiter)
-  # )
-  #   
-  prompt <-
-    glue::glue(
-      "You are a classification AI. Determine, for each of the following ", length(x) ," texts which of the options best matches. ",
-      "The texts are:\n\n",
-      paste0("\"", x, collapse = "\"\n"), "\"",
-      "\n\nThe options are:\n",
-      paste(options, collapse = "\n"), 
-      "\n\nNo capitalization. No explanations. Write only one option for each text, i.e. ", length(x), " responses."
-    )
-  
-  
-    out <- model$chat(prompt)
-    
-    output_vector <- out |> stringr::str_split("\n") |> unlist() |> trimws()
-    
-    if(length(output_vector) != length(x)) {
-      stop(paste0("output vector length is ", length(output_vector), " but expect length ", length(x)))
-    }
-    
-    return(output_vector)
-    
+# model <- chat_groq(model = "deepseek-r1-distill-llama-70b",
+#                    system_prompt = write_intial_prompt(options))
+
+jsonlite::fromJSON(prompts_list[[1]])
+
+
+for (i in vectors) {
+  output_list[[i]] <- model$chat(list_x[[i]], echo = FALSE) 
+  # we are capped at 30 requests or 6000 tokens per minute under Groq free service
+  # waiting x seconds between requests to try and reduce token use
+  Sys.sleep(10)
 }
 
-
-x <- cod_data$D 
-
-# smoking categories of hazard ratio
-
-# the options are taken from the following paper...
-
-# https://pmc.ncbi.nlm.nih.gov/articles/PMC3229033/#:~:text=Current%20smokers%20had%20significantly%20higher,23.93)%2C%20smoking%2Drelated%20cancers
+# convert the output back into dataframes...
+output_list[1:4] |> purrr::map(function(x) x |> stringr::str_remove_all("\`") |> stringr::str_remove("json") |> jsonlite::fromJSON())
 
 
-options <- 
-c(
-"coronary heart disease",
-"cerebrovascular disease",
-"sudden death",
-"pulmonary disease",
-"lung cancer",
-"colorectal cancer",
-"larynx cancer", 
-"kidney cancer", 
-"acute myeloid leukemia", 
-"oral cavity cancer", 
-"esophageal cancer", 
-"pancreatic cancer", 
-"bladder cancer", 
-"stomach cancer",
-"prostate cancer",
-"none"
+
+# check the length of each output vector
+purrr::map_dbl(output_list, length)
+
+# collect results into a dataframe
+data.frame(
+  list_x |> unlist(),
+  output_list |> unlist()
 )
 
-
-classifcations <- classification_prompt(x = x, options = options, model = llama_model)
-
-data.frame(
-  x,
-  classifiers
-) |> View()
+output_list[[i]] 
--- prompt_writing_functions.R
+++ prompt_writing_functions.R
@@ -1,16 +1,23 @@
 
-x <- x |> stringr::str_remove_all("[)(:,]")
-
-model = llama_model
-
 # function to write the intial prompt for classification
-write_intial_prompt <- function(options) {
+# write_intial_prompt <- function(options) {
+# 
+#   glue::glue(
+#     "You are a classification LLM. For every line of text you receive, you will only return which of the options best matches. ",
+#     "\n\nThe options are:\n",
+#     paste0("\"", options, collapse = "\"\n"), "\"",
+#     "\n\nNo capitalization. No explanations. Write only one of the above options for each line of text you are given."
+#   )
+#   
+# }
 
+write_intial_prompt <- function(options) {
+  
   glue::glue(
-    "You are a classification AI. Determine, for each text sent, return only which of the options best matches. ",
-    "\n\nThe options are:\n",
+    "You are a classification LLM. You will receive a JSON file. The file will contain a list of items with cause_of_death.
+    You must only return the edited version of this JSON file. Please add 'category' to each item, which can only ever have one of the following values:\n",
     paste0("\"", options, collapse = "\"\n"), "\"",
-    "\n\nNo capitalization. No explanations. Write only one of the above options for each line of text you are given."
+    "\n\nNo capitalization. No explanations. Return only the data in a structured JSON format."
   )
   
 }
@@ -25,48 +32,45 @@ split_vector <- function(vec, max_length_per_vec) {
   split(vec, ceiling(seq_along(vec) / max_length_per_vec))
 }
 
-model$chat(options |> write_intial_prompt())
 
-length(x) 
+glue_chr_vector <- function(vec) {
+  glue::glue(paste0("\"", vec, collapse = "\"\n"), "\"")
+}
 
-list_x <- x |> split_vector(10)
-
-prompts_list <- list_x |> purrr::map(glue_chr_vector)
-
-vectors <- seq_along(prompts_list)
-output_list <- vector("list", length = length(prompts_list))
-
-for (i in vectors) {
-  output_list[[i]] <- model$chat(prompts_list[[i]]) |> stringr::str_split("\n") |> unlist() |> trimws()
-  Sys.sleep(2)
+glue_to_json <- function(vec) {
+  glue::glue('[\n{glue::glue_collapse(glue::glue(\'  {{ "cause_of_death": "{vec}" }}\'), ",\n")}\n]')
 }
 
 
 
-
-
-# function to create send classification request to LLM and return results in vector
-classification_prompt <- function(x, options, model = llama_model, delimiter = "\n") {
-  
-  model$chat(options |> write_intial_prompt())
-    
-  output_vector <- out |> stringr::str_split("\n") |> unlist() |> trimws()
-  
-  if(length(output_vector) != length(x)) {
-    stop(paste0("output vector length is ", length(output_vector), " but expect length ", length(x)))
-  }
-  
-  return(output_vector)
-  
-}
-
-
-
- 
-cod_subset <- x |> sample(10) |> stringr::str_remove_all("[)(:,]")
-
-out <- model$chat(items_prompt(cod_subset))
-
-x <- x |> sample(10) |> stringr::str_remove_all("[)(:,]")
-
-classifier_vec <- out |> stringr::str_split("\n") |> unlist()
+my_json <-  "{\n  \"diseases\": [\"typhoid fever\", \"salmonella enteritis\", \"salmonella sepsis\", \"localized salmonella infections\", \"salmonella infection unspecified\"],\n  \"category\": \"none\"\n}"
+jsonlite::fromJSON(my_json)
+# 
+# 
+# 
+# 
+# # function to create send classification request to LLM and return results in vector
+# classification_prompt <- function(x, options, model = llama_model, delimiter = "\n") {
+#   
+#   model$chat(options |> write_intial_prompt())
+#     
+#   output_vector <- out |> stringr::str_split("\n") |> unlist() |> trimws()
+#   
+#   if(length(output_vector) != length(x)) {
+#     stop(paste0("output vector length is ", length(output_vector), " but expect length ", length(x)))
+#   }
+#   
+#   return(output_vector)
+#   
+# }
+# 
+# 
+# 
+#  
+# cod_subset <- x |> sample(10) |> stringr::str_remove_all("[)(:,]")
+# 
+# out <- model$chat(items_prompt(cod_subset))
+# 
+# x <- x |> sample(10) |> stringr::str_remove_all("[)(:,]")
+# 
+# classifier_vec <- out |> stringr::str_split("\n") |> unlist()
